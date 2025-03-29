971ac802a6d1803da8fdedb47178f12eb3e87f5f
Mohamed Elnady <163608574+Mo-elnady2021@users.noreply.github.com>
2025-03-29 08:34:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
-import express from "express";
-import bcrypt from "bcrypt";
+import express from 'express'
+import bcrypt from 'bcrypt'
 
-const app = express();
-const port = 3000;
 
-// In-memory
-const users = [];
+const app =express()
+const port =3001;
 
-app.use(express.json());
+//in-mermory
+const users=[];
 
-app.post("/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    // Find user
-    const findUser = users.find((data) => email == data.email);
-    if (findUser) {
-      res.status(400).send("Wrong email or password !");
-    }
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({ email, password: hashedPassword });
-    console.log(users)
-    res.status(201).send("Registered successfully!");
-  } catch (err) {
-    res.status(500).send({ message: err.message });
+app.use(express.json())
+
+///try&catch-error(Register)
+
+app.post('/register', async (req,res) =>{
+//try
+try{
+    const {email, password} =req.body 
+//find-users
+const findUser = users.find((data) => email ==data.email)
+  if (findUser) {
+    res.status(400).send(" wrong email & password !")
   }
-});
+  //Hash-Password-Using-(Bcrypt)
+const hashedPassword = await bcrypt.hash(password, 10)
+users.push({email,password:hashedPassword});
+console.log(users)
+res.status(201).send("Registered Successfully!")
+}catch{
+res.status(500).send({message: err.message });
+}
+})
+//end-register
 
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    // Find user
-    const findUser = users.find((data) => email == data.email);
-    if (!findUser) {
-      res.status(400).send("Wrong email or password !");
-    }
-    const passwordMatch = await bcrypt.compare(password, findUser.password);
-    if(passwordMatch) {
-        res.status(200).send("Logged in successfully!")
-    } else {
-        res.status(400).send("Wrong email or password !");
-    }
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
-
+/// try&catch-(login)
+app.post("/login", async (req,res) =>
+{
+ try{
+ const { email,password } = req.body;
+ //find-users
+const findUser = users.find((data) => email ==data.email)
+if (findUser) {
+  res.status(400).send(" wrong email & password !")}
+const passwordMatch = await bcrypt.compare(password,findUser.password)
+if(passwordMatch)
+    res.status(200).send("log in successfully");
+else {res.status(400).send(" wrong email & password !")}
+    
+ } catch (err) {
+    res.status(500).send({message: err.message });
+ }
+})
+//end-login
 app.listen(port, () => {
-  console.log("Server is started on port 3000");
-});
+    console.log("Server is Started on Port 3001")
+} )
